30fcb0d8a586fe13e8d90d301cc4545316ed2d3b
Hiroshi-N-S <Hiroshi.N.September11@gmail.com>
2025-06-02 19:22:26 +0900

    map `realm roles` to `groups` in `Keycloak`

--- terraform/keycloak/users.tf
+++ terraform/keycloak/users.tf
@@ -5,8 +5,23 @@
 locals {
   groups      = {
     admin: {
+      role_ids = [
+        keycloak_role.harbor_roles["harbor-admin"].id,
+        keycloak_role.argo_cd_roles["argo-cd-admin"].id,
+        keycloak_role.prometheus_grafana_roles["prometheus-grafana-admin"].id
+      ]
       additional_group_ids = [
         keycloak_group.minio_groups["minio-consoleAdmin"].id
+      ]
+    }
+    viewer: {
+      role_ids = [
+        keycloak_role.harbor_roles["harbor-user"].id,
+        keycloak_role.argo_cd_roles["argo-cd-readonly"].id,
+        keycloak_role.prometheus_grafana_roles["prometheus-grafana-viewer"].id
+      ]
+      additional_group_ids = [
+        keycloak_group.minio_groups["minio-readonly"].id
       ]
     }
   }
@@ -39,6 +54,18 @@ resource "keycloak_group" "devops_groups" {
 
   realm_id    = keycloak_realm.devops.id
   name        = each.key
+}
+
+#
+# Group-Roles for `devops` realm
+#
+
+resource "keycloak_group_roles" "devops_group_roles" {
+  for_each    = local.groups
+
+  realm_id    = keycloak_realm.devops.id
+  group_id    = keycloak_group.devops_groups[each.key].id
+  role_ids    = each.value.role_ids
 }
 
 #
